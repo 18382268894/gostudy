e9ad4a1c1f2022f066d27da0300fe5a7685c3bd2
18382268894 <qq1005914310@live.com>
2018-05-12 11:30:00 +0800

    test

--- day9/main/main.go
+++ day9/main/main.go
@@ -11,7 +11,5 @@ func main() {
 	var opStack = postfix.NewStack()   //符号栈
 	var str string = "21*(93+6/3-51)+4-19"
 	postfix.DealStr(str, exps, opStack)
-	expString := exps.ToString()
-	fmt.Printf("表达式：%v",exps)
-    fmt.Printf("%s",expString)
+	fmt.Printf("表达式：%v", exps)
 }
